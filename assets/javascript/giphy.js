86f8b2a74c35c799bad700d57c0c95dbf11f7724
emssslay <emily.robinson@iechs.org>
2019-03-12 12:14:05 -0600

    starting up

---
+++ assets/javascript/giphy.js
@@ -0,0 +1,36 @@
+// array of strings as "topics" or topic
+var anime = ["", "", "", ""];
+
+// put animes on the page as buttons (for loop)
+
+
+
+// link the api using ajax
+
+
+
+// when the user clicks the button, we want 10 giphys to come back. 
+
+
+
+// add limit of 10 giphys 
+
+
+
+// all giphys should be static at first 
+
+
+
+// create on click function to go from static to animated s
+
+
+
+// show the giphy rating under each giphy 
+
+
+
+
+
+
+
+// add search option (as a form) to be able to add more buttons based on the input - last step
